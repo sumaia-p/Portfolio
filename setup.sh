2dff9de43147953cf95237353a6503e170f8f7cd
sumaia-p <88396101+sumaia-p@users.noreply.github.com>
2021-08-03 11:20:35 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "[theme]
-primaryColor=’#020202’
-backgroundColor=’#c4c3c3’
-secondaryBackgroundColor=’#ebd316’
-font = ‘sans serif’
-[server]
-headless = true
-port = $PORT
-enableCORS = false
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
